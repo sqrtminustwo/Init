6699322e19ff098c43f08906df38087c36462849
Maksym Ziborov <Maksym.Ziborov@UGent.be>
2025-07-15 00:46:04 +0200

    screenshots added

--- src/cppinit.py
+++ src/cppinit.py
@@ -3,21 +3,15 @@ import sys
 import os
 
 project_name = None
-base_dir = os.getcwd()
+base_dir = None
 
 try:
     project_name = sys.argv[1]
-    if len(sys.argv) >= 3:
-        try:
-            base_dir = sys.argv[2]
-        except:
-            print('Invalid base dir')
-
-    print(project_name, base_dir)
+    base_dir = os.getcwd()
 except:
     print('Usage: ./cppinit project_name [Base dir path]')
 
-if (project_name is not None) or (base_dir is not None):
+if project_name is not None:
     init = Initializer(base_dir, project_name);
 else:
     print(f'Invlaid project name {project_name} or base_dir {base_dir}')
